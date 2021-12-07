11b6c2ffd557180af506be8363df7e17da458b62
Broder Peters <broder.peters@protonmail.com>
2021-12-07 12:02:08 +0100

    CU-k13xc0: Upgrade terraform to 0.13.7

--- versions.tf
+++ versions.tf
@@ -1,3 +1,11 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13.7"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+  }
 }
